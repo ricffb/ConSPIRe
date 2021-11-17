3b9e17d483367f706014b0ca20ad65505bbf40ea
Henrik Wachowitz <>
2021-11-17 09:47:41 +0100

    extend exp typechecks

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,9 +1,12 @@
 {-# LANGUAGE TupleSections #-}
+{-# OPTIONS_GHC -Wno-missing-fields #-}
+
+-- {-# LANGUAGE LambdaCase #-}
 
 module Lib where
 
 import CSPM.Parser
-import CSPM.Syntax (Action (..), ActionI (..), Construct (..), Exp (Brack, Dot, Eq, Var), Proc (..), Programm)
+import CSPM.Syntax (Action (..), ActionI (..), Construct (..), Exp (..), Literal (LBool, LInt, LVar), Proc (..), Programm, Type (..))
 import Control.Monad.Except
   ( ExceptT,
     MonadError (throwError),
@@ -16,7 +19,11 @@ import Control.Monad.Reader
   )
 import Control.Monad.State (State)
 import Data.Bifoldable
-import qualified Data.HashMap.Lazy as Map
+import Data.Bifunctor (second)
+-- import qualified Data.HashMap.Lazy as Map
+
+import Data.Map ((!?))
+import qualified Data.Map as Map
 import Data.Tuple.Extra (both)
 import Debug.Trace (trace)
 import Text.Show (Show)
@@ -24,26 +31,13 @@ import Text.Show (Show)
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
 
-type SumT a = (String, a)
-
-data Type
-  = TProc Type
-  | TFun Type Type
-  | TInd String Type
-  | TNum
-  | TBool
-  | TSum [SumT Type]
-  | TProd [Type]
-  | TVar String
-  deriving (Show, Eq)
-
 data TypeError
   = TypeMismatch Type Type
   | NotFunction Type
   | NotInScope String
   | NotExpression String
   | NotChannel String Type
-  deriving (Show)
+  deriving (Show, Eq)
 
 pEmpty :: Type
 pEmpty = TProd []
@@ -51,26 +45,27 @@ pEmpty = TProd []
 sEmpty :: Type
 sEmpty = TSum []
 
-tBool :: Type
-tBool = TSum [("true", pEmpty), ("false", pEmpty)]
-
-type CanBeEvaluated = Either Type Proc
-
-type Env = [(String, CanBeEvaluated)]
+data Env = Env
+  { typeEnv :: Map.Map String Type,
+    procEnv :: Map.Map String Proc
+  }
 
 type Check = ExceptT TypeError (Reader Env)
 
 addToEnv :: (String, Type) -> Check a -> Check a
-addToEnv (n, t) = local (\env -> (n, Left t) : env)
+addToEnv (n, t) = local (\env -> Env {typeEnv = Map.insert n t (typeEnv env)})
+
+addToEnv' :: [(String, Type)] -> Check a -> Check a
+addToEnv' xs = local (\env -> Env {typeEnv = Map.fromList xs `Map.union` typeEnv env})
 
 -- Return a tuple of (var, type) pairs, fst -> Outputs, snd -> new Variables
-findTypesOfChannel :: Action -> Type -> Check ([(String, Type)], [(String, Type)])
+findTypesOfChannel :: Action -> Type -> Check ([(Exp, Type)], [(String, Type)])
 findTypesOfChannel (chan, items) t@(TSum sum) =
   case lookup chan sum of
     Nothing -> throwError $ NotChannel chan t
     Just t -> matchItems t items
   where
-    matchItems :: Type -> [ActionI] -> Check ([(String, Type)], [(String, Type)])
+    matchItems :: Type -> [ActionI] -> Check ([(Exp, Type)], [(String, Type)])
     matchItems t@(TProd typs) actions =
       if length typs /= length actions
         then throwError $ NotChannel ("The number of Arguments is different" ++ show actions) t
@@ -108,15 +103,18 @@ check p t = case p of
   --
   CallProc process args -> do
     env <- ask
-    case lookup process env of
-      Just (Left t) -> return t
-      Just (Right p) -> addToEnv (process, TProc t) $ check p t
-      Nothing -> throwError $ NotInScope process
+    let pEnv = procEnv env
+    let tEnv = typeEnv env
+    case tEnv !? process of
+      Just t -> return t
+      Nothing -> case pEnv !? process of
+        Nothing -> throwError $ NotInScope process
+        Just pr -> addToEnv (process, TProc t) $ check pr t
   --
   Prefix action pr -> do
     (outputs, vars) <- findTypesOfChannel action t
-
-    return $ trace ("Vars: " ++ show vars) $ TProc t
+    outputTypes <- mapM (uncurry checkExpHasType) outputs
+    addToEnv' vars $ check pr t
   --
   ExtChoice pleft pright -> do
     lhs <- check pleft t
@@ -141,53 +139,83 @@ check p t = case p of
       _ -> throwError $ TypeMismatch lhs (TProc t)
   --
   Ite exp pleft pright -> do
-    et <- checkExp exp
+    et <- checkExpHasType exp TBool
     lhs <- check pleft t
     rhs <- check pright t
-    if et /= TBool
-      then throwError $ TypeMismatch et TBool
-      else case lhs of
-        TProc ty
-          | ty == t -> case rhs of
-            TProc ty'
-              | ty' == t -> return $ TProc t
-            _ -> throwError $ TypeMismatch rhs (TProc t)
-        _ -> throwError $ TypeMismatch lhs (TProc t)
+    case lhs of
+      TProc ty
+        | ty == t -> case rhs of
+          TProc ty'
+            | ty' == t -> return $ TProc t
+          _ -> throwError $ TypeMismatch rhs (TProc t)
+      _ -> throwError $ TypeMismatch lhs (TProc t)
   --
-  Seq pr pr' -> undefined
+  Seq pr pr' -> do
+    p1Type <- check pr t
+    check pr' t
   Parallel set pr pr' -> undefined
   Hide set pr -> undefined
   ReplIntChoice s set pr -> undefined
+  PCaseExpr exp cases -> undefined
+  PLambda arg typ pr -> undefined
+  Let var exp pr -> undefined
 
+checkExpHasType :: Exp -> Type -> Check Type
+checkExpHasType exp t = do
+  inferredType <- checkExp exp
+  if inferredType == t
+    then return t
+    else throwError $ TypeMismatch inferredType t
+
+-- Check Expression for type
 checkExp :: Exp -> Check Type
 checkExp exp = case exp of
   Eq exp' exp2 -> do
     rhs <- checkExp exp'
     lhs <- checkExp exp2
     if rhs == lhs then return TBool else throwError $ TypeMismatch rhs lhs
-  Dot exp' exp2 -> do
-    rhs <- checkExp exp'
-    lhs <- checkExp exp2
-    return $ TProd [rhs, lhs]
-  Var s -> do
+  Lit s -> checkLit s
+  App fun arg -> do
+    tfun <- checkExp fun
+    targ <- checkExp arg
+    case tfun of
+      TFun argT retT -> if argT == targ then return retT else throwError $ TypeMismatch argT targ
+      _ -> throwError $ NotFunction tfun
+  ELambda argname typename expr -> undefined --do
+  --retT <- addToEnv (argname, typename) $ checkExp expr
+  -- return TFun T Type
+  ECaseExpr exp cases -> undefined
+  Tuple exprs -> do
+    typs <- mapM checkExp exprs
+    return $ TProd typs
+
+checkLit :: Literal -> Check Type
+checkLit l = case l of
+  LInt n -> return TNum
+  LVar s -> do
     env <- ask
-    case lookup s env of
+    case typeEnv env !? s of
       Nothing -> throwError $ NotInScope s
-      Just (Right e) -> throwError $ NotExpression s
-      Just (Left t) -> return t
-  Brack exp' -> checkExp exp'
+      Just nt -> return nt
+  LBool b -> return TBool
+
+tBool :: Type
+tBool = TSum [("true", pEmpty), ("false", pEmpty)]
 
 trivial :: Proc
-trivial = ExtChoice (Ite (Eq (Var "s") (Var "s'")) (CallProc "P" []) STOP) (CallProc "Q" [])
+trivial = ExtChoice (Ite (Eq (Lit $ LVar "s") (Lit $ LVar "s'")) (CallProc "P" []) STOP) (CallProc "Q" [])
+
+withVarIte :: Proc
+withVarIte = Ite (Lit $ LVar "y") SKIP STOP
 
 withPrefix :: Proc
-withPrefix = Prefix ("in", [Input "x", Input "y"]) SKIP
+withPrefix = Prefix ("in", [Input "x", Input "y"]) $ Prefix ("out", [Output $ Eq (Lit $ LVar "x") (Lit $ LVar "x")]) withVarIte
 
 prefixT :: Type
-prefixT = TSum [("in", TProd [tBool, tBool]), ("out", tBool)]
+prefixT = TSum [("in", TProd [tBool, TBool]), ("out", TBool)]
 
-trivEnv :: Env
-trivEnv = [("s", Left $ TVar "X"), ("s'", Left $ TVar "X"), ("P", Left (TProc tBool)), ("Q", Right (ExtChoice (CallProc "P" []) STOP))]
+--trivEnv :: Env
+--trivEnv = [("s", Left $ TVar "X"), ("s'", Left $ TVar "X"), ("P", Left (TProc tBool)), ("Q", Right (ExtChoice (CallProc "P" []) STOP))]
 
 runCheck :: Env -> Check a -> Either TypeError a
 runCheck env = flip runReader env . runExceptT
