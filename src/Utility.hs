9c719edfd2c8a56f27e2791c3456dcd3780f41ee
Henrik Wachowitz <ricffb@gmail.com>
2021-11-30 09:50:21 +0000

    add uneither

--- src/Utility.hs
+++ src/Utility.hs
@@ -3,3 +3,7 @@ module Utility (safeHead) where
 safeHead :: [a] -> Maybe a
 safeHead (x : _) = Just x
 safeHead _ = Nothing
+
+unEither :: Either a a -> a
+unEither (Right a) = a
+unEither (Left a) = a
