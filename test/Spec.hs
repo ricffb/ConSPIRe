e04f430ade780950334f742ff792ec7e2b9c2de6
Henrik Wachowitz <ricffb@gmail.com>
2021-11-30 09:50:12 +0000

    add test with fold and type var expansion

--- test/Spec.hs
+++ test/Spec.hs
@@ -13,6 +13,8 @@ main =
         checkTop emptyEnv withPrefix prefixT `shouldBe` (Right (TProc prefixT) :: Either TypeError Type)
       it "can handle case in expression" $
         let alph = alphabet withECaseEnv in checkTop withECaseEnv withECaseExpr alph `shouldBe` (Right (TProc alph) :: Either TypeError Type)
+      it "can deal with inductive types" $
+        let alph = alphabet foldEnv in checkTop foldEnv foldProc alph `shouldBe` (Right (TProc alph) :: Either TypeError Type)
 
 tBool :: Type
 tBool = TSum [("myTrue", pEmpty), ("myFalse", pEmpty)]
@@ -31,14 +33,15 @@ withPrefix = Prefix ("in", [Input "x", Input "y"]) $ Prefix ("out", [Output $ Eq
 
 -- let f = \ u : U -> case u of myTrue -> \t : E -> true of myFalse -> \t : E -> false within in?u -> if f u then SKIP else STOP
 withECaseExpr :: Proc
-withECaseExpr = Let "f" (ELambda "u" "U" (ECaseExpr (Lit (LVar "u")) [ECase "myFalse" (ELambda "t" "E" (Lit (LBool False))), ECase "myTrue" (ELambda "t" "E" (Lit (LBool True)))])) (Prefix ("in", [Input "u"]) (Ite (App (Lit (LVar "f")) (Lit (LVar "u"))) SKIP STOP))
+withECaseExpr = Let "f" (ELambda "u" (TVar "U") (ECaseExpr (Lit (LVar "u")) [ECase "myFalse" (ELambda "t" (TVar "E") (Lit (LBool False))), ECase "myTrue" (ELambda "t" (TVar "E") (Lit (LBool True)))])) (Prefix ("in", [Input "u"]) (Ite (App (Lit (LVar "f")) (Lit (LVar "u"))) SKIP STOP))
 
 withECaseEnv :: Env
 withECaseEnv =
   Env
     { alphabet = TSum [("in", tBool)],
       typeEnv = Map.fromList [("U", tBool), ("E", pEmpty)],
-      procEnv = Map.empty
+      procEnv = Map.empty,
+      exprEnv = Map.empty
     }
 
 prefixT :: Type
@@ -49,5 +52,22 @@ trivEnv =
   Env
     { alphabet = tBool,
       typeEnv = Map.fromList [("s", TVar "X"), ("s'", TVar "X"), ("P", TProc tBool)],
-      procEnv = Map.singleton "Q" $ ExtChoice (CallProc "P" []) STOP
+      procEnv = Map.singleton "Q" $ ExtChoice (CallProc "P" []) STOP,
+      exprEnv = Map.empty
     }
+
+foldEnv :: Env
+foldEnv =
+  Env
+    { alphabet = TSum [("asklen", TVar "MyList"), ("retlen", TVar "Int")],
+      typeEnv = Map.fromList [("MyList", TInd "L" (TSum [("nil", TProd []), ("cons", TProd [TVar "Bool", TVar "L"])])), ("Bool", TBool), ("Int", TNum), ("snd", TFun (TProd [TVar "Bool", TVar "Int"]) (TVar "Int"))],
+      procEnv = Map.empty,
+      exprEnv =
+        Map.fromList
+          [ ("ll", Sum "cons" (Tuple [Lit (LBool True), Sum "cons" (Tuple [Lit (LBool False), Sum "nil" (Tuple [])])])),
+            ("calcLen", ELambda "xs" (TSum [("nil", TProd []), ("cons", TProd [TVar "Bool", TVar "Int"])]) (ECaseExpr (Lit (LVar "xs")) [ECase "cons" (ELambda "tpl" (TProd [TVar "Bool", TVar "Int"]) (MathOp [Lit (LInt 1), App (Lit (LVar "snd")) (Lit (LVar "tpl"))])), ECase "nil" (ELambda "y" (TProd []) (Lit (LInt 0)))]))
+          ]
+    }
+
+foldProc :: Proc
+foldProc = Prefix ("asklen", [Input "list"]) (Let "len" (Fold (Lit (LVar "list")) (Lit (LVar "calcLen"))) (Prefix ("retlen", [Output (Lit (LVar "len"))]) SKIP))
