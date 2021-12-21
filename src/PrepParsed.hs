b6027ee129cbccb004da9755c89341fd4669c7c2
Henrik Wachowitz <>
2021-12-21 17:53:15 +0100

    fix prepEnv

--- src/PrepParsed.hs
+++ src/PrepParsed.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE NamedFieldPuns #-}
 
-module PrepParsed where
+module PrepParsed (prepEnv) where
 
 import CSPM.Syntax (Construct (Assert, NamedExpr, NamedProc, NamedType, TypeVar), Proc' (PLambda), Type (TProd, TVar))
 import Data.Foldable (foldl')
@@ -11,6 +11,7 @@ import StdLib (stdTypeEnv)
 -- Transform a named proc with args into a Lambda Proc without args
 tfNamedProc :: Construct -> Construct
 tfNamedProc np@(NamedProc name [] pr) = np
+tfNamedProc np@(NamedProc name [(argName, typ)] pr) = NamedProc name [] $ PLambda [argName] typ pr
 tfNamedProc (NamedProc name args pr) = NamedProc name [] newPr
   where
     argNames = fst <$> args
@@ -24,7 +25,7 @@ prepEnv = foldl' (flip $ collectEnv . tfNamedProc) stdEnv
     stdEnv = emptyEnv {typeEnv = stdTypeEnv}
     collectEnv :: Construct -> Env -> Env
     collectEnv constr env@Env {typeEnv, procEnv, exprEnv} = case constr of
-      TypeVar s -> let newtEnv = Map.insert s (TVar s) typeEnv in env {typeEnv = newtEnv}
+      TypeVar s -> let newtEnv = Map.insert s (TVar s) typeEnv in env --{typeEnv = newtEnv}
       NamedType s ty -> let newtEnv = Map.insert s ty typeEnv in env {typeEnv = newtEnv}
       NamedExpr s ex -> let neweEnv = Map.insert s ex exprEnv in env {exprEnv = neweEnv}
       Assert {} -> env
